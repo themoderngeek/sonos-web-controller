9de3801c6b9ea1691ffea531ba36d51754b21387
Mark Taylor <moderngeekish@gmail.com>
2017-02-18 14:26:31 +0000

    Inital project structure

---
+++ src/main/java/uk/co/themoderngeek/SonosWebApplication.java
@@ -0,0 +1,12 @@
+package uk.co.themoderngeek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonosWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonosWebApplication.class, args);
+	}
+}
